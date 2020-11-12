448666a0b3c49e709b6cb7bbc8d82536e6615b67
Frands Otting <frands@otting.dk>
2020-11-12 20:15:01 +0100

    remove <C-æ> (c-;) mapping

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -12,7 +12,7 @@ hs.hotkey.bind(
 
 keyUpDown = function(modifiers, key)
     -- Un-comment & reload config to log each keystroke that we're triggering
-    -- log.d('Sending keystroke:', hs.inspect(modifiers), key)
+    log.d("Sending keystroke:", hs.inspect(modifiers), key)
 
     hs.eventtap.keyStroke(modifiers, key, 0)
 end
