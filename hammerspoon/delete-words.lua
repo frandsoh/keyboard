bea9750a154c3a16d07d7d592a559132c369f93d
Frands Otting <frands@otting.dk>
2020-11-14 13:41:50 +0100

    play around with terminal filter

--- hammerspoon/delete-words.lua
+++ hammerspoon/delete-words.lua
@@ -6,26 +6,49 @@ local isInTerminal = function()
 end
 
 -- Use option + h to delete previous word
+-- hs.hotkey.bind(
+--   {"alt"},
+--   "h",
+--   function()
+--     if isInTerminal() then
+--       keyUpDown({"ctrl"}, "w")
+--     else
+--       keyUpDown({"alt"}, "delete")
+--     end
+--   end
+-- )
+
 hs.hotkey.bind(
   {"alt"},
   "h",
   function()
     if isInTerminal() then
-      keyUpDown({"ctrl"}, "w")
+      keyUpDown({"alt"}, "h")
     else
       keyUpDown({"alt"}, "delete")
     end
   end
 )
-
 -- Use option + l to delete next word
+-- hs.hotkey.bind(
+--   {"alt"},
+--   "l",
+--   function()
+--     if isInTerminal() then
+--       keyUpDown({"ctrl"}, ";")
+--       keyUpDown({}, "d")
+--     else
+--       keyUpDown({"alt"}, "forwarddelete")
+--     end
+--   end
+-- )
+
 hs.hotkey.bind(
   {"alt"},
   "l",
   function()
     if isInTerminal() then
-      keyUpDown({}, "escape")
-      keyUpDown({}, "d")
+      keyUpDown({"alt"}, "l")
     else
       keyUpDown({"alt"}, "forwarddelete")
     end
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,19 +2,19 @@ local log = hs.logger.new("init.lua", "debug")
 
 -- Use Control+` to reload Hammerspoon config
 hs.hotkey.bind(
-    {"ctrl"},
-    "`",
-    nil,
-    function()
-        hs.reload()
-    end
+  {"ctrl"},
+  "`",
+  nil,
+  function()
+    hs.reload()
+  end
 )
 
 keyUpDown = function(modifiers, key)
-    -- Un-comment & reload config to log each keystroke that we're triggering
-    log.d("Sending keystroke:", hs.inspect(modifiers), key)
+  -- Un-comment & reload config to log each keystroke that we're triggering
+  log.d("Sending keystroke:", hs.inspect(modifiers), key)
 
-    hs.eventtap.keyStroke(modifiers, key, 0)
+  hs.eventtap.keyStroke(modifiers, key, 0)
 end
 
 -- Subscribe to the necessary events on the given window filter such that the
@@ -27,19 +27,19 @@ end
 --
 -- Returns nothing.
 enableHotkeyForWindowsMatchingFilter = function(windowFilter, hotkey)
-    windowFilter:subscribe(
-        hs.window.filter.windowFocused,
-        function()
-            hotkey:enable()
-        end
-    )
+  windowFilter:subscribe(
+    hs.window.filter.windowFocused,
+    function()
+      hotkey:enable()
+    end
+  )
 
-    windowFilter:subscribe(
-        hs.window.filter.windowUnfocused,
-        function()
-            hotkey:disable()
-        end
-    )
+  windowFilter:subscribe(
+    hs.window.filter.windowUnfocused,
+    function()
+      hotkey:disable()
+    end
+  )
 end
 
 require("keyboard.control-escape")
